bd4ba6dc3b6a23a94186b0cf5b69b9afe2a7f005
tobrutbai <tobrutbaik8152@gmail.com>
2024-11-20 21:02:24 +0700

    Add files via upload

---
+++ all/security/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6289508082845',
+    'text': "oke",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6289508082845',
+  'filePath': "./all/approval",
+  'checkFilePath': "../setting/osaragi.js",
+  'codeToDetect': 'main();'
+};
