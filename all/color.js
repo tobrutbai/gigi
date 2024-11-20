07219354501fb9b5853896c071644c5fcd720c10
tobrutbai <tobrutbaik8152@gmail.com>
2024-11-20 21:06:22 +0700

    Add files via upload

---
+++ all/color.js
@@ -0,0 +1,22 @@
+// BASE CREATE BY RUZTANXD 
+
+// Osaragi V1.0
+// YT: QyuuNee
+
+require("./module")
+
+const color = (text, color) => {
+    return !color ? chalk.green(text) : chalk.keyword(color)(text)
+}
+
+const bgcolor = (text, bgcolor) => {
+	return !bgcolor ? chalk.green(text) : chalk.bgKeyword(bgcolor)(text)
+}
+
+const sleep = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+global.color = color
+global.bgcolor = bgcolor
+global.sleep = sleep
